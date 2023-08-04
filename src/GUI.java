e8c9f398a84ae7bd85160a4a18428d537b8710a3
joeljabez0134 <joeldavethu@gmail.com>
2023-08-04 13:26:34 +0800

    for review

--- src/GUI.java
+++ src/GUI.java
@@ -6,14 +6,14 @@ public class GUI {
         // Getting the user input from a GUI
         String name = JOptionPane.showInputDialog("What's your name?");
         JOptionPane.showMessageDialog(null, "Hello " + name);
-//
-//        int age = Integer.parseInt(JOptionPane.showInputDialog("Age: "));
-//        JOptionPane.showMessageDialog(null, "Your age is " + age);
-//
-//        float height = Float.parseFloat(JOptionPane.showInputDialog("Height: "));
-//        JOptionPane.showMessageDialog(null, "Your height is " + height);
 
-//      // More info about GUI
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Age: "));
+        JOptionPane.showMessageDialog(null, "Your age is " + age);
+
+        float height = Float.parseFloat(JOptionPane.showInputDialog("Height: "));
+        JOptionPane.showMessageDialog(null, "Your height is " + height);
+
+        // More info about GUI
         JOptionPane.showMessageDialog(null, "Hello " + name, "Hello title", JOptionPane.INFORMATION_MESSAGE);
         JOptionPane.showMessageDialog(null, "Hello " + name, "Hello title", JOptionPane.PLAIN_MESSAGE);
         JOptionPane.showMessageDialog(null, "Hello " + name, "Hello title", JOptionPane.ERROR_MESSAGE);
---
+++ src/TDD/Main.java
@@ -0,0 +1,11 @@
+package TDD;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("#");
+        System.out.println("#");
+        System.out.println("#");
+        System.out.println("#");
+        System.out.println("#");
+    }
+}
